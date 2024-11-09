3f30858056db129e8eda78f1383c45d2e31ef0f8
otachloeee <e0775128@u.nus.edu>
2024-11-10 02:17:37 +0800

    Updated extraction file with changes

--- entity_relationship_extraction/entity_extraction/longer_entity_rs_extraction_parallelized.py
+++ entity_relationship_extraction/entity_extraction/longer_entity_rs_extraction_parallelized.py
@@ -235,7 +235,7 @@ def extract_entities_rs(csv_file_path):
 # Main execution
 if __name__ == '__main__':
     # Extract from existing cleaned datasets 
-    # csv_file_path = '../../backend/data/00 - mock_student_data.csv'
+    csv_file_path = '../../backend/data/00 - mock_student_data.csv'
     # csv_file_path = '../../backend/data/01 - mock_module_info.csv'
     # csv_file_path = '../../backend/data/02 - mock_department_list.csv'
     # csv_file_path = '../../backend/data/03 - mock_staff_info.csv'
@@ -243,7 +243,7 @@ if __name__ == '__main__':
     # csv_file_path = '../../backend/data/05 - mock_venue_info.csv'
     # csv_file_path = '../../backend/data/06 - nus_undergraduate_programmes.csv'
     # csv_file_path = '../../backend/data/07 - Jobs and relevant skillset (linkedin).csv'
-    csv_file_path = '../../backend/data/08 - jobs_and_tech (ONET).csv'
+    # csv_file_path = '../../backend/data/08 - jobs_and_tech (ONET).csv'
     # csv_file_path = '../../backend/data/09 - jobs_and_skills (ONET).csv'
     # csv_file_path = '../../backend/data/10 - Graduate Employment Survey.csv'
     
@@ -252,7 +252,7 @@ if __name__ == '__main__':
     
     # Save results
     base_name, ext = os.path.splitext(os.path.basename(csv_file_path))
-    new_file_name = f"TEST{base_name}_extracted{ext}"
+    new_file_name = f"{base_name}_extracted{ext}"
     df.to_csv(new_file_name, index=False)
     
     # Print a success message with the new file name
